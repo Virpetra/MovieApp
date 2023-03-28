a41b9db58f2ab6e242fde6ab14090200d25c147f
Virpetra <msaiddede@gmail.com>
2023-03-28 18:16:13 +0300

    Initial Commit

---
+++ MovieApp(Main)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieApp(Main)
+//
+//  Created by Mehmet Said Dede on 28.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieApp(Main)/MovieApp_Main_App.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp_Main_App.swift
+//  MovieApp(Main)
+//
+//  Created by Mehmet Said Dede on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp_Main_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
